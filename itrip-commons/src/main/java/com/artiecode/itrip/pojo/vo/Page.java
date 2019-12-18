d238400fb86d9de9aa94a263bdf2da3c6b9356c1
Arthuriman <arthuriman@163.com>
2019-12-18 12:31:18 +0800

    增加搜索模块，同时看看你们的机子如何应对，就问扎不扎你

---
+++ itrip-commons/src/main/java/com/artiecode/itrip/pojo/vo/Page.java
@@ -0,0 +1,140 @@
+package com.artiecode.itrip.pojo.vo;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * <b>系统分页对象</b>
+ * @param <T>
+ * @author Arthur
+ * @version 3.1.1
+ * @since 3.1.1
+ */
+public class Page<T> implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private Integer curPage;
+	/**
+	 * 总记录数 .
+	 */
+	private Integer total;
+	/**
+	 * 每页行数 .
+	 */
+	private Integer pageSize;
+	/**
+	 * 页面的总数  .
+	 */
+	private Integer pageCount;
+	/**
+	 * 结果集中数据的起始位置  .
+	 */
+	private Integer beginPos;
+	/**
+	 * List 集合.
+	 */
+	private List<T> rows;
+
+	public Page() {
+
+	}
+	/**
+	 * 当前页面 .
+	 * 页面的大小 .
+	 * @param curpage .
+	 * @param pagesize .
+	 */
+	public Page(int curpage, Integer pagesize) {
+		this.curPage = curpage;
+		this.pageSize = pagesize;
+	}
+	/**
+	 * @param curpage .
+	 * @param total .
+	 * @param pagesize .
+	 */
+	public Page(int curpage,Integer pagesize,Integer total) {
+		super();
+		this.curPage = curpage;//当前页码
+		this.total = total;//总记录数
+		this.pageSize = pagesize;//页码容量
+		//总页数=总记录数total/pageSize（+1）
+		this.pageCount = (total + this.pageSize - 1) /this.pageSize;
+		//下标起始位置：(curPage-1)*pageSize
+		this.beginPos = (curPage-1)*pageSize;
+	}
+	/**
+	 * 总页面数 .
+	 * @return Integer .
+	 */
+	public Integer getPageCount() {
+		return pageCount;
+	}
+	/**
+	 *  得到页面的当前位置 .
+	 * @return Integer .
+	 */
+	public Integer getBeginPos() {
+		return beginPos;
+	}
+	/**
+	 * @param curPage
+	 *            the curPage to set
+	 */
+	public void setCurPage(int curPage) {
+		this.curPage = curPage;
+	}
+
+	public List<T> getRows() {
+		return rows;
+	}
+
+	public void setRows(List<T> rows) {
+		this.rows = rows;
+	}
+	/**
+	 * @param pageSize
+	 *            the pageSize to set
+	 */
+	public void setPageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	/**
+	 * @param rowCount
+	 *            the rowCount to set
+	 */
+	public void setRowCount(Integer rowCount) {
+		this.total = rowCount;
+	}
+
+	/**
+	 * @param beginPos
+	 *            the beginPos to set
+	 */
+	public void setBeginPos(Integer beginPos) {
+		this.beginPos = beginPos;
+	}
+	public Integer getTotal() {
+		return total;
+	}
+
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
+
+	public void setPageCount(Integer pageCount) {
+		this.pageCount = pageCount;
+	}
+
+	public Integer getCurPage() {
+		return curPage;
+	}
+
+	public void setCurPage(Integer curPage) {
+		this.curPage = curPage;
+	}
+
+	public Integer getPageSize() {
+		return pageSize;
+	}
+}
---
+++ itrip-search-consumer/src/main/java/com/artiecode/itrip/controller/SearchController.java
@@ -0,0 +1,15 @@
+package com.artiecode.itrip.controller;
+
+import com.artiecode.itrip.base.controller.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <b>搜索模块控制器</b>
+ * @author Arthur
+ * @version 3.1.1
+ * @since 3.1.1
+ */
+@RequestMapping("/search")
+public class SearchController extends BaseController {
+
+}
